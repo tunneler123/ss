4964e011f80dd64eadd75944325d097a6fa5492d
tunneler123 <55013273+tunneler123@users.noreply.github.com>
2020-09-14 16:20:23 +0800

    Update ssocks.sh

--- ssocks.sh
+++ ssocks.sh
@@ -5,7 +5,7 @@ screen -dmS screen ssserver -p 10 -k tunneler -m aes-256-cfb
 MYIP=`curl -s ifconfig.me`;
 MYIP2="s/xxxxxxxxx/$MYIP/g";
 wget -O /etc/shadows.txt "https://raw.githubusercontent.com/tunneler123/ss/master/shadows.txt"
-sed -i $MYIP2 /etc/shadow.txt
+sed -i $MYIP2 /etc/shadows.txt
 clear
 echo -e "\e[1;32m SHADOWSOCKS INSTALLED \e[0m"
 echo -e "\e[1;32m CHECK AT /etc/shadows.txt \e[0m"
