5aa33c245cdef7ecdda21c28a7aa74734c44ec60
tunneler123 <55013273+tunneler123@users.noreply.github.com>
2020-09-14 16:33:23 +0800

    Update ssocks.sh

--- ssocks.sh
+++ ssocks.sh
@@ -1,7 +1,7 @@
 apt-get install shadowsocks
 apt-get install screen
 apt-get install curl
-screen -dmS screen ssserver -p 10 -k tunneler -m aes-256-cfb
+screen -dmS screen ssserver -p 11 -k tunneler -m aes-256-cfb
 MYIP=`curl -s ifconfig.me`;
 MYIP2="s/xxxxxxxxx/$MYIP/g";
 wget -O /etc/shadows.txt "https://raw.githubusercontent.com/tunneler123/ss/master/shadows.txt"
